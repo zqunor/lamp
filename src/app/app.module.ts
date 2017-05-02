e8f01d56e607c83a13bd8292e1511db25fa16f68
zqunor <zqunor@gmail.com>
2017-05-02 20:10:59 +0800

    lamp add menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { PlannerPage } from '../pages/planner/planner';
 import { TimePage } from '../pages/time/time';
 import { EfficiencyPage } from '../pages/efficiency/efficiency';
 import { CalendarPage } from '../pages/calendar/calendar';
+import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,14 +17,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     PlannerPage,
     TimePage,
     EfficiencyPage,
-    CalendarPage
+    CalendarPage,
+    ListPage
   ],
   imports: [
     BrowserModule,
@@ -35,14 +31,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     PlannerPage,
     TimePage,
     EfficiencyPage,
-    CalendarPage
+    CalendarPage,
+    ListPage
   ],
   providers: [
     StatusBar,
