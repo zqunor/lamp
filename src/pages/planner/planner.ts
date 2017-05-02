f1eba62c0d1e2f9099d6ebdd01d8860b9d75f862
zqunor <zqunor@gmail.com>
2017-05-02 19:24:30 +0800

    lamp with tabs

---
+++ src/pages/planner/planner.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-planner',
+  templateUrl: 'planner.html'
+})
+export class PlannerPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
---
+++ src/pages/time/time.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-time',
+  templateUrl: 'time.html'
+})
+export class TimePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
